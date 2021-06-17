963080ef0852610f8623cd2370e5893c88bf7242
Garima <gammo0508@gmail.com>
2021-06-17 17:29:19 +0530

    stage one

---
+++ app.js
@@ -0,0 +1,68 @@
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+const app = express(); // creating app obj
+
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // response.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    " Origin, Accept, X-Requested-With, Content-Type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+// creating database and placeing connection ${process.env.DB_USER} ${process.env.DB_PASSWORD} ${process.env.DB_NAME}
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2moh9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("connection done");
+  })
+  .catch((err) => {
+    console.log("FAILED");
+    console.log(err);
+  });
---
+++ util/location.js
@@ -0,0 +1,7 @@
+async function getCoordsForAddress(address) {
+  return {
+    lat: 40.783355,
+    lng: -73.098578,
+  };
+}
+module.exports = getCoordsForAddress;
