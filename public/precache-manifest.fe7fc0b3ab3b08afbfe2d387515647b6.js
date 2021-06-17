385e6ff00c2458d3ea544ab1556fe769b079c4bc
Garima <gammo0508@gmail.com>
2021-06-17 19:47:29 +0530

    added react

---
+++ public/precache-manifest.fe7fc0b3ab3b08afbfe2d387515647b6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf4cba49b53beda38805ecf5684c6f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f27082ebe6b88f28123b",
+    "url": "/static/css/0.f2ebe16b.chunk.css"
+  },
+  {
+    "revision": "6444f5223150eeb873f8",
+    "url": "/static/css/4.ea00bc80.chunk.css"
+  },
+  {
+    "revision": "77448d95d97cc22a217c",
+    "url": "/static/css/5.24132c3b.chunk.css"
+  },
+  {
+    "revision": "820757ae40d72923feec",
+    "url": "/static/css/6.992ddf32.chunk.css"
+  },
+  {
+    "revision": "e8cb0b496d1ba0980932",
+    "url": "/static/css/7.77ccd530.chunk.css"
+  },
+  {
+    "revision": "6835eab8c6566ecfe5c2",
+    "url": "/static/css/8.bbd6bbca.chunk.css"
+  },
+  {
+    "revision": "5afe0588215110272ba3",
+    "url": "/static/css/main.15d6bab1.chunk.css"
+  },
+  {
+    "revision": "f27082ebe6b88f28123b",
+    "url": "/static/js/0.64ae0ebb.chunk.js"
+  },
+  {
+    "revision": "352c99a54e8c7f189f54",
+    "url": "/static/js/3.ed7edd00.chunk.js"
+  },
+  {
+    "revision": "6444f5223150eeb873f8",
+    "url": "/static/js/4.2c9ea883.chunk.js"
+  },
+  {
+    "revision": "77448d95d97cc22a217c",
+    "url": "/static/js/5.13620658.chunk.js"
+  },
+  {
+    "revision": "820757ae40d72923feec",
+    "url": "/static/js/6.834ee6c6.chunk.js"
+  },
+  {
+    "revision": "e8cb0b496d1ba0980932",
+    "url": "/static/js/7.71d5d0c1.chunk.js"
+  },
+  {
+    "revision": "6835eab8c6566ecfe5c2",
+    "url": "/static/js/8.822040be.chunk.js"
+  },
+  {
+    "revision": "5afe0588215110272ba3",
+    "url": "/static/js/main.1bf47b1a.chunk.js"
+  },
+  {
+    "revision": "8a7b9326bffc3dfb80ea",
+    "url": "/static/js/runtime-main.773fa1a2.js"
+  }
+]);
